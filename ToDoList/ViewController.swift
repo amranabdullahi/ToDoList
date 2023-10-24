9fe6f0af0412f97a2513ea46ecbe98db6b578a5a
Amran <124218@swcsd.us>
2023-10-24 08:16:55 -0400

    Initial Commit

---
+++ ToDoList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Amran Abdullahi on 10/24/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
